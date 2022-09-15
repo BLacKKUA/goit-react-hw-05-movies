41675c0214874f10a2ccf160ce07c541a1bff7c0
Roman Valko <rvalko.dev@gmail.com>
2022-09-15 13:03:40 +0300

    done

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -6,7 +6,6 @@ export const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
   const location = useLocation();
-  const checkLocation = location.state?.from ?? '/';
   const params = useParams();
   console.log(params);
   useEffect(() => {
@@ -22,7 +21,7 @@ export const MovieDetails = () => {
   return (
     <>
       <div>
-        <NavLink to={checkLocation}>Go to back</NavLink>
+        <NavLink to={location.state?.from ?? '/'}>Go to back</NavLink>
         <h1>{movie.name ?? movie.title}</h1>
         <img
           src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`}
@@ -39,7 +38,11 @@ export const MovieDetails = () => {
         <h4>Additional information</h4>
         {NavItem.map(button => {
           return (
-            <NavLink key={`${button.href}`} to={`${button.href}`}>
+            <NavLink
+              key={`${button.href}`}
+              to={`${button.href}`}
+              state={location.state?.from ?? '/'}
+            >
               {button.name}
             </NavLink>
           );
